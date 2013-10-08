179c7fd2054c0c33fa1d3272f0f551e6a877520e
Richard <Richard@Richard-PC>
2013-10-08 01:02:56 -0400

    Second sketches

--- src/eightqueens/EightQueens.java
+++ src/eightqueens/EightQueens.java
@@ -1,48 +1,64 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package eightqueens;
 
-/**
- *
- * @author Richard
- */
+import java.util.Scanner;
+
 public class EightQueens {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        System.out.println("Welcome to the EightQueens program!");
-        
-        //Creates an 8x8 chess board that holds strings 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to the Eight Queens program!");
+        System.out.println("Please enter in the X cordinate"
+                + " of the first Queen");
+        int FirstX = input.nextInt();
+        System.out.println("Please enter in the Y cordinate");
+        int FirstY = input.nextInt();
+
+        //Creates an 8x8 chess board that holds strings
         String[][] ChessBoard = new String[8][8];
-        
-        //sum(5);
-        System.out.println(sum(3));
-        
-        
-    }
-    
-    //Lets try a basic recursive loop
-    //This loop returns the factorial sum of every number less than it
-    public static int sum(int num){
-        int result;
-        if (num == 1){
-            result = 1;
-        }else{
-            result = num + sum(num-1);
+
+        //A loop to fill the 
+        //entire thing with asterixes
+        for (int i = 0; i < 8; i++) {
+            for (int y = 0; y < 8; y++) {
+                ChessBoard[i][y] = "*";
+
+            }
         }
-        return result;
+        //Puts down the first Queen
+        ChessBoard[FirstX][FirstY] = "Q";
+
+
+
+        //Print the whole thing out    
+        for (int i = 0; i < 8; i++) {
+            System.out.print("\n");
+            for (int y = 0; y < 8; y++) {
+                System.out.print(" " + ChessBoard[y][i]);
+            }
+        }
+
+
     }
-    
+
     /*
-    Hint: to determine whether setting q[n] = i conflicts with q[0] through q[n-1] 
-    if q[i] equals q[n]: two queens are placed in the same column
-    if q[i] - q[n] equals n - i: two queens are on same major diagonal
-    if q[n] - q[i] equals n - i: two queens are on same minor diagonal 
+     Hint: to determine whether setting q[n] = i conflicts with q[0] through q[n-1] 
+     if q[i] equals q[n]: two queens are placed in the same column
+     if q[i] - q[n] equals n - i: two queens are on same major diagonal
+     if q[n] - q[i] equals n - i: two queens are on same minor diagonal 
      */
+    public void PrintItOut() {
+    }
 }
+/*
+ //Lets try a basic recursive loop
+ //This loop returns the factorial sum of every number less than it
+ public static int sum(int num) {
+ int result;
+ if (num == 1) {
+ result = 1;
+ } else {
+ result = num + sum(num - 1);
+ }
+ return result;
+ } */
