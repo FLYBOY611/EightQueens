b1aaa0f5d9d9d4786495e1a8d39e1850ee086d05
Richard <Richard@Richard-PC>
2013-10-10 01:18:55 -0400

    Closer........

--- src/eightqueens/CreateBoard.java
+++ src/eightqueens/CreateBoard.java
@@ -33,30 +33,30 @@ public class CreateBoard {
     //The start of our recursion
     public boolean PlaceAQueen(int Xstart, int Ystart) {
         //int CountStart = Xstart;
-        int RowSpot = 0;
+        int CurrentColumn = 0;
         boolean KeepGoing = false;
 
         if (Ystart == ChessBoard.length) {
             return true;
         } else {
-            while (RowSpot < ChessBoard.length && !KeepGoing == true) {
+            while (CurrentColumn < ChessBoard.length && !KeepGoing == true) {
                 //If the spot is a threat, 
                 //test the next spot to put a Queen
-                if (IsAThreat(RowSpot, Ystart) == true) {
-                    RowSpot++;
-                    //RowSpot++;
+                if (IsAThreat(CurrentColumn, Ystart) == true) {
+                    //Ystart++;
+                    CurrentColumn++;
                 } else {
                     //If its not a threat then
                     //Place a Queen and do recursion
                     //to try and place another Queen in next column.
-                    ChessBoard[RowSpot][Ystart] = "Q";
-                    KeepGoing = PlaceAQueen(RowSpot, Ystart + 1);
+                    ChessBoard[Xstart][CurrentColumn] = "Q";
+                    KeepGoing = PlaceAQueen(CurrentColumn, Ystart + 1);
 
-                     /*    //BACKTRACKING
+               /*    //BACKTRACKING
                      //Remove the Queen 
                      if (KeepGoing == true) {                       
-                     ChessBoard[RowSpot][Ystart] = "*";
-                     RowSpot++;
+                     ChessBoard[CurrentColumn][Ystart] = "*";
+                     CurrentColumn++;
                      }  */
 
                 }
@@ -68,51 +68,81 @@ public class CreateBoard {
     //Determine if the new piece threatens another
     public boolean IsAThreat(int row, int column) {
 
-        //Test the row threat
+//Test the row threat
         for (int i = 0; i < ChessBoard.length; i++) {
-            if (ChessBoard[row][i] == "Q") {
+            if (ChessBoard[row][i].equals("Q")) {
                 return true;
             }
         }
-        
-        //Test the diagonal threat trending upwards
+
+//Test the column threat
         for (int i = 0; i < ChessBoard.length; i++) {
-            if (row+i >= ChessBoard.length
-                    || column-i < 0){
-                System.out.println("broken!");
+            if (ChessBoard[i][column].equals("Q")) {
+                return true;
+            }
+        }
+
+
+//Test the diagonal threat trending upward right
+        for (int i = 0; i < ChessBoard.length; i++) {
+            if (row + i >= ChessBoard.length
+                    || column + i >= ChessBoard.length) {
+                // System.out.println("broken!");
                 break;
             }
-            if (ChessBoard[row+i][column-i].equals("Q")) {
+            if (ChessBoard[row + i][column + i].equals("Q")) {
                 return true;
             }
         }
-        
-        //Test the diagonal threat trending downwards
-        for (int i = ChessBoard.length; i > 0; i--) {
-            if (row-i < 0
-                    || column-i < 0){
+
+//Test the diagonal threat trending downward right
+        for (int i = 0; i < ChessBoard.length; i++) {
+            if (row + i >= ChessBoard.length
+                    || column - i < 0) {
+                // System.out.println("broken!");
                 break;
             }
-            if (ChessBoard[row-i][column-i].equals("Q")) {
+            if (ChessBoard[row + i][column - i].equals("Q")) {
                 return true;
             }
         }
-        
-        
+
+//Test the diagonal threat trending upward left
+        for (int i = 0; i < ChessBoard.length; i++) {
+            if (row - i < 0
+                    || column + i >= ChessBoard.length) {
+                break;
+            }
+            if (ChessBoard[row - i][column + i].equals("Q")) {
+                return true;
+            }
+        }
+
+//Test the diagonal threat trending downward left
+        for (int i = 0; i < ChessBoard.length; i++) {
+            if (row - i < 0
+                    || column - i < 0) {
+                break;
+            }
+            if (ChessBoard[row - i][column - i].equals("Q")) {
+                return true;
+            }
+        }
+
+
         return false;
     }
 
     //Print the whole thing out 
     public void PrintBoard() {
-        for (int i = 0; i < 8; i++) {
+        for (int y = 7; y >= 0; y--) {
             System.out.print("\n");
-            for (int y = 0; y < 8; y++) {
-                System.out.print(" " + ChessBoard[y][i]);
+            for (int x = 0; x < 8; x++) {
+                System.out.print(" " + ChessBoard[x][y]);
             }
         }
     }
 }
-
 
 /*
  Hint: to determine whether setting q[n] = i conflicts with q[0] through q[n-1] 
